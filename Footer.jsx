f4b3505281900ec4a9fe87541c3b51767ff8bc37
Princebaghel <70906613+Princebaghel@users.noreply.github.com>
2021-06-07 17:33:46 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(){
+  var currYear = new Date().getFullYear();
+  return(
+    <footer>
+      <p>copy right @ 2021</p>
+    </footer>    
+  );
+}
+  export default Footer;
